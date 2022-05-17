e1230cb083a253de0a84a452969301d6182ab44f
qureshi_ <iftekharqureshi100@gmail.com>
2022-05-17 22:40:48 +1000

    firstcommit

---
+++ WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {   
+            IEnumerable<Category>objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
