8caf8636822380c37c08ba1cc863c1c4a264895b
qureshi_ <iftekharqureshi100@gmail.com>
2022-06-03 00:26:31 +1000

    Implementing all CRUD Operations

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -40,5 +40,72 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var objProductfromDb = _db.Products.Find(id);
+/*            var objProductDbFirst = _db.Products.FirstOrDefault(u => u.Id == id);
+            var objProductDbSingle = _db.Products.SingleOrDefault(u => u.Id == id);*/
+            
+            if(objProductfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(objProductfromDb);
+        }
+
+        //POST
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            //Editing the database
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var objProductfromDb = _db.Products.Find(id);
+            /*            var objProductDbFirst = _db.Products.FirstOrDefault(u => u.Id == id);
+                        var objProductDbSingle = _db.Products.SingleOrDefault(u => u.Id == id);*/
+
+            if (objProductfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(objProductfromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var objProductfromDb = _db.Products.Find(id);
+
+            if (objProductfromDb == null)
+            {
+                return NotFound();
+            }
+            //Deleting from the database
+            _db.Products.Remove(objProductfromDb);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -7,6 +7,8 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        [Range(1,10,ErrorMessage="Quantity should be less than 10!")]
         public int Quantity { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
